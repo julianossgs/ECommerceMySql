caf0b093d8eb8ab8f00a7787c00b51626998fdea
Juliano Candido <julianosgs@yahoo.com.br>
2022-09-08 09:18:09 -0300

    Projeto ECommerce MySql

---
+++ ECommerce.sql
@@ -0,0 +1,11 @@
+create database mini_ec;
+use mini_ec;
+#cadastros independentes
+
+#Usuarios
+create table usuarios(
+id_user int not null primary key auto_increment,
+nome_user varchar(50) not null,
+email_user varchar()
+);
+
